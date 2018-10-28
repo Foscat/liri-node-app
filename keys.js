4304f019e73fc9cf9da561c6ccc71aba078bc009
Kyle Foster <40616718+Foscat@users.noreply.github.com>
2018-10-28 18:25:01 -0500

    working protected app
    has key files to protect passwords

---
+++ keys.js
@@ -0,0 +1,12 @@
+exports.spotify = {
+    id: process.env.SPOTIFY_ID,
+    secret: process.env.SPOTIFY_SECRET
+}
+
+exports.omdb = {
+    apikey: process.env.apikey
+}
+
+exports.bandsintown = {
+    APP_ID: process.env.APP_ID
+}
